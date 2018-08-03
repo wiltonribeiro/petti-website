9690769aa41929a9bc50595f1dd2b53889330a38
Wilton Neto <wiltonribeiro@users.noreply.github.com>
2018-08-03 18:58:43 -0300

    correção de bugs

--- src/views/Activities/Sections/Description.js
+++ src/views/Activities/Sections/Description.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Parallax from "../../../components/Parallax";
 import background from "../../../images/paulo.JPG"
+import backgroundMobile from "../../../images/lucas.jpg"
 import Hidden from '@material-ui/core/Hidden';
 
 const styles = {
@@ -12,14 +13,26 @@ class Description extends Component {
   render() {
     return (
         <div>
-            <Parallax opacity="0" size="250px" img={background} content={
-                <div style={{textAlign: "center", color:"#fff", fontFamily:"Raleway"}}>
-                    <p style={{fontSize:"50px"}}><Hidden  only={['sm', 'xs']}>Nossas </Hidden>Atividades</p>
-                    <Hidden  only={['sm', 'xs']}>
-                        <h4 style={{marginTop:"-50px"}}>Divididas em Ensino, Pesquisa e Extensão</h4>
-                    </Hidden>
-                </div>
-            } />
+            <Hidden  only={['sm', 'xs']}>
+                <Parallax opacity="0" size="250px" img={background} content={
+                    <div style={{textAlign: "center", color:"#fff", fontFamily:"Raleway"}}>
+                        <p style={{fontSize:"50px"}}><Hidden  only={['sm', 'xs']}>Nossas </Hidden>Atividades</p>
+                        <Hidden  only={['sm', 'xs']}>
+                            <h4 style={{marginTop:"-50px"}}>Divididas em Ensino, Pesquisa e Extensão</h4>
+                        </Hidden>
+                    </div>
+                } />
+            </Hidden>
+            <Hidden  only={['lg','xl','md']}>
+                <Parallax opacity="0" size="250px" img={backgroundMobile} content={
+                    <div style={{textAlign: "center", color:"#fff", fontFamily:"Raleway"}}>
+                        <p style={{fontSize:"50px"}}><Hidden  only={['sm', 'xs']}>Nossas </Hidden>Atividades</p>
+                        <Hidden  only={['sm', 'xs']}>
+                            <h4 style={{marginTop:"-50px"}}>Divididas em Ensino, Pesquisa e Extensão</h4>
+                        </Hidden>
+                    </div>
+                } />
+            </Hidden>
         </div>
     );
   }
--- src/views/Documents/Sections/Description.js
+++ src/views/Documents/Sections/Description.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Parallax from "../../../components/Parallax";
 import background from "../../../images/credenciamento.jpg"
+import backgroundMobile from "../../../images/credenciamento2.jpg"
 import Hidden from '@material-ui/core/Hidden';
 
 const styles = {
@@ -12,14 +13,28 @@ class Description extends Component {
   render() {
     return (
         <div>
-            <Parallax opacity="0" size="250px" img={background} content={
-                <div style={{textAlign: "center", color:"#fff", fontFamily:"Raleway"}}>
-                    <p style={{fontSize:"50px"}}>Editais <Hidden  only={['sm', 'xs']}>e Documentos</Hidden></p>
-                    <Hidden  only={['sm', 'xs']}>
-                        <h4 style={{marginTop:"-50px"}}>Editais, Notas e Documentos relacionados a nós</h4>
-                    </Hidden>
-                </div>
-            } />
+            <Hidden  only={['sm', 'xs']}>
+                <Parallax opacity="0" size="250px" img={background} content={
+                    <div style={{textAlign: "center", color:"#fff", fontFamily:"Raleway"}}>
+                        <p style={{fontSize:"50px"}}>Editais <Hidden  only={['sm', 'xs']}>e Documentos</Hidden></p>
+                        <Hidden  only={['sm', 'xs']}>
+                            <h4 style={{marginTop:"-50px"}}>Editais, Notas e Documentos relacionados a nós</h4>
+                        </Hidden>
+                    </div>
+                } />
+            </Hidden>
+
+            <Hidden  only={['lg','xl','md']}>
+                <Parallax opacity="0" size="250px" img={backgroundMobile} content={
+                    <div style={{textAlign: "center", color:"#fff", fontFamily:"Raleway"}}>
+                        <p style={{fontSize:"50px"}}>Editais <Hidden  only={['sm', 'xs']}>e Documentos</Hidden></p>
+                        <Hidden  only={['sm', 'xs']}>
+                            <h4 style={{marginTop:"-50px"}}>Editais, Notas e Documentos relacionados a nós</h4>
+                        </Hidden>
+                    </div>
+                } />
+            </Hidden>
+
         </div>
     );
   }
